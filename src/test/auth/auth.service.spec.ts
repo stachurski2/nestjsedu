17ae2163914fb9c1f1f71c9427620b36e86d8250
Stan <stanislawsobczyk@icloud.com>
2023-09-28 18:58:45 +0200

    setup tests for auth

--- src/test/auth/auth.service.spec.ts
+++ src/test/auth/auth.service.spec.ts
@@ -1,4 +1,33 @@
+import { ConfigModule } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthController } from '../../auth/auth.controller';
+import { AuthService } from '../../auth/auth.service';
+import { UsersRepository } from '../../auth/users.repository'
+
 describe('AuthController', () => {
+  
+  let authService: AuthService;
+  let usersRepository;
+  
+  const mockUsersRepository = () => ({
+    getTasks: jest.fn(),
+    findOne: jest.fn(),
+  });
+  
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        { provide: UsersRepository, useFactory: mockUsersRepository },
+        { provide: JwtService, useValue: {} },
+      ],
+    }).compile();
+    authService = await module.get(AuthService);
+    usersRepository = await module.get(UsersRepository);
+  });
+  
   it('someTest', async () => {
     expect(true).toEqual(true);
   });
